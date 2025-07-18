dc4bd0e21f8e99656565dd9200f479828fd756e0
ctrlahr <ctrlahrium@gmail.com>
2025-07-18 10:51:16 -0300

    feat: Initial commit

---
+++ src/main/java/com/jorgeLuis/Restaurant/reservarion/system/RestaurantReservarionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeLuis.Restaurant.reservarion.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservarionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservarionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgeLuis/Restaurant/reservarion/system/RestaurantReservarionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeLuis.Restaurant.reservarion.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservarionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

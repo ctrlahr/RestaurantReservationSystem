568a670923cf11da3ad2de6a7df0019919cf799d
ctrlahr <ctrlahrium@gmail.com>
2025-07-20 12:53:26 -0300

    feat: add the list for id method

--- src/main/java/com/jorgeLuis/Restaurant/reservarion/system/Mesas/MesasController.java
+++ src/main/java/com/jorgeLuis/Restaurant/reservarion/system/Mesas/MesasController.java
@@ -1,6 +1,7 @@
 package com.jorgeLuis.Restaurant.reservarion.system.Mesas;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("mesas")
 public class MesasController {
 
+    private MesasService mesasService;
+
+    public MesasController(MesasService mesasService) {
+        this.mesasService = mesasService;
+    }
+
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Boas vindas a tabela de mesas";
+    }
+
+    @GetMapping("/listarmesasid/{id}")
+    public MesasDTO listarMesasPorId(@PathVariable Long id) {
+        return mesasService.listarMesasPorId(id);
     }
 
 }
--- src/main/java/com/jorgeLuis/Restaurant/reservarion/system/Mesas/MesasService.java
+++ src/main/java/com/jorgeLuis/Restaurant/reservarion/system/Mesas/MesasService.java
@@ -2,9 +2,26 @@ package com.jorgeLuis.Restaurant.reservarion.system.Mesas;
 
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MesasService {
 
+    private MesasRepository mesasRepository;
+    private MesasMapper mesasMapper;
+
+    public MesasService(MesasRepository mesasRepository, MesasMapper mesasMapper) {
+        this.mesasRepository = mesasRepository;
+        this.mesasMapper = mesasMapper;
+    }
 
 
+
+//    Método de listar todas as mesas
+public MesasDTO listarMesasPorId(Long id) {
+        Optional<MesasModel> mesa = mesasRepository.findById(id);
+        return mesa.map(mesasMapper::map).orElse(null);
+}
 }
